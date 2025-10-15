f96ac5fe168c9dd972f1e085b7afc909ef4e176c
DG-code101 <davidgamikon@gmail.com>
2025-10-15 19:20:25 +0100

    Add files via upload

---
+++ code/first/main.cpp
@@ -0,0 +1,13 @@
+//My first code :D
+#include <iostream>
+using namespace std;
+
+int main(){
+string myName = "David";
+string myUni = "Baze University";
+string myCourse = "Computer Science.";
+int myAge = 17;
+cout << "Hello world\n";
+cout << "My Name is " << myName << " and I am a student of " << myUni << " studying " << myCourse << " I am currently " << myAge << " years old.";
+return 0;
+}
